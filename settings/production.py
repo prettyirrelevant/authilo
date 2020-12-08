9a0622c0f4eee2ccc35224157cfa13808ebfe5ff
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-08 12:17:38 +0100

    fixed minor issh

--- settings/production.py
+++ settings/production.py
@@ -1,5 +1,7 @@
 from .development import *
 from os import environ
 
-SECRET_KEY = environ.get('SECRET_KEY')
-MONGO_URI = environ.get('DATABASE_URL')
+SECRET_KEY = environ.get("SECRET_KEY")
+MONGO_URI = environ.get("DATABASE_URL")
+DEBUG = environ.get("DEBUG")
+ENV = environ.get("ENV")
